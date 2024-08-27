d3f242fbecbb38b5b38e2004451e0f84f1c24a9c
sayoridev <mliguori798@gmail.com>
2024-08-27 17:18:36 +0200

    add default extension & -h flag

--- removeback.py
+++ removeback.py
@@ -1,20 +1,37 @@
 #!/bin/python3
 
 import sys
+import argparse
 from rembg import remove
 from PIL import Image
+import os
 
 def removeback(pinput, poutput):
+    """Remove the background from the input image and save the result to the output path."""
     with Image.open(pinput) as img:
         img_without_back = remove(img)
+
+        # Add a default extension if not provided
+        root, ext = os.path.splitext(poutput)
+        if not ext:
+            ext = '.png'
+            poutput = root + ext
+        
         img_without_back.save(poutput)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Uso: removeback <imagine input> <imagine output>")
-        sys.exit(1)
+def main():
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description="Remove background from an image.")
     
-    pinput = sys.argv[1]
-    poutput = sys.argv[2]
+    # Add the input and output file arguments
+    parser.add_argument('input', type=str, help='Path to the input image')
+    parser.add_argument('output', type=str, help='Path to save the output image with background removed')
 
-    removeback(pinput, poutput)
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Call the remove background function with the provided arguments
+    removeback(args.input, args.output)
+
+if __name__ == "__main__":
+    main()
